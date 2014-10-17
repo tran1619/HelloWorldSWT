18ec4f58030c1ad8104654ae6ffc1297aa0416fe
tran1619 <tran1619@gmail.com>
2014-10-17 15:16:48 -0400

    Change to use row layout.

--- HelloWorldSWT/src/HelloWorldSWT.java
+++ HelloWorldSWT/src/HelloWorldSWT.java
@@ -1,3 +1,6 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowLayout	;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
@@ -9,7 +12,23 @@ public class HelloWorldSWT {
 		Display display = new Display();
 		Shell shell = new Shell(display);
 		shell.setText("Hello World!");
+		
+		//FillLayout fillLayout = new FillLayout(SWT.HORIZONTAL);
+		
+		// Width of horizontal margins placed along the left and right edges
+		// fillLayout.marginWidth = 5;
+		// Height of Vertical margins place along the top and bottom edges
+		// fillLayout.marginHeight = 5;
+		
+		shell.setLayout(new RowLayout());
+		
+		Button buttonHello = new Button(shell, SWT.PUSH);
+		buttonHello.setText("Hello There");
+		
+		shell.setSize(450, 100);
+		
 		shell.open();
+		
 		while(!shell.isDisposed()) {
 			if(!display.readAndDispatch())
 				display.sleep();
